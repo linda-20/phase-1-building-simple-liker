a14ca35c17963bb3145e8f35f4616b14fa77ad0c
linda-20 <lindaaron40@gmail.com>
2024-01-15 12:23:09 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.querySelector('#modal');
+errorModal.classList.add('hidden');
+
+// Function to toggle the heart state
+function toggleHeartState(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        errorModal.classList.remove('hidden');
+        const errorMessage = document.querySelector('#modal-message');
+        errorMessage.textContent = error;
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  } else {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+// Add click event listeners to all hearts
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach((heart) => {
+  heart.addEventListener('click', () => {
+    toggleHeartState(heart);
+  });
+});
 
 
 
